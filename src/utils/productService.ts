14d7b98ea35c372871a9d206a18d5595078701d7
dianaomr <dian.omrani@gmail.com>
2025-05-02 02:45:18 +0430

    fix: update with fetchbrands part

--- src/utils/productService.ts
+++ src/utils/productService.ts
@@ -15,29 +15,35 @@ export interface Product {
   }
   
 
-const apiUrl = 'https://67faf6938ee14a542628f55b.mockapi.io/products';
+// const apiUrl = 'https://67faf6938ee14a542628f55b.mockapi.io/products';
+const baseUrl = 'https://67faf6938ee14a542628f55b.mockapi.io/products';
+
+export const fetchBrands = async (): Promise<{ name: string; image: string }[]> => {
+  const response = await axios.get(baseUrl); 
+  return response.data;
+};
 
 //  دریافت محصولات
 export const fetchProducts = async (): Promise<Product[]> => {
-  const response = await axios.get(apiUrl);
+  const response = await axios.get(baseUrl);
   return response.data;
 };
 
 //  افزودن محصول جدید
 export const addProduct = async (product: Product): Promise<Product> => {
-  const response = await axios.post(apiUrl, product);
+  const response = await axios.post(baseUrl, product);
   return response.data;
 };
 
 //  ویرایش محصول
 export const updateProduct = async (product: Product): Promise<Product> => {
   if (!product.id) throw new Error('Product ID is required for update');
-  const response = await axios.put(`${apiUrl}/${product.id}`, product);
+  const response = await axios.put(`${baseUrl}/${product.id}`, product);
   return response.data;
 };
 
 //  حذف محصول
 export const deleteProduct = async (productId: string): Promise<string> => {
-  const response = await axios.delete(`${apiUrl}/${productId}`);
+  const response = await axios.delete(`${baseUrl}/${productId}`);
   return response.data;
 };
