744ff5c1c712c9988578de82e384526c6c3e6725
dianaomr <dian.omrani@gmail.com>
2025-05-06 20:17:39 +0430

    fix: update redux store

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,5 +1,4 @@
 
-// src/redux/store.ts
 import { configureStore } from '@reduxjs/toolkit';
 import productReducer from './slices/productSlice';
 import quantityReducer from './slices/quantitySlice';
@@ -7,6 +6,30 @@ import filterReducer from "./slices/filterSlice";
 import cartReducer from "./slices/cartSlice";
 import orderReducer from "./slices/orderSlice";
 import orderDoneReducer from "./slices/orderDoneSlice";
+import latestOrderReducer from "./slices/latestOrderSlice";
+import favoriteReducer from "./slices/favoriteSlice";
+
+const loadCartFromLocalStorage = () => {
+  try {
+    const serializedState = localStorage.getItem("cart");
+    return serializedState ? JSON.parse(serializedState) : undefined;
+  } catch (err) {
+    console.warn("Load cart failed:", err);
+    return undefined;
+  }
+};
+
+const saveCartToLocalStorage = (state: any) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("cart", serializedState);
+  } catch (err) {
+    console.warn("Save cart failed:", err);
+  }
+};
+
+const preloadedCartState = loadCartFromLocalStorage();
+// ------------------------------------------------------
 
 export const store = configureStore({
   reducer: {
@@ -16,7 +39,17 @@ export const store = configureStore({
     cart: cartReducer,
     orders: orderReducer,
     orderDone: orderDoneReducer,
+    latestOrder: latestOrderReducer,
+    favorites: favoriteReducer,
   },
+  preloadedState: {
+    cart: preloadedCartState, 
+  },
+});
+
+// هر بار که استیت cart تغییر کرد، در localStorage ذخیره 
+store.subscribe(() => {
+  saveCartToLocalStorage(store.getState().cart);
 });
 
 export type RootState = ReturnType<typeof store.getState>;
