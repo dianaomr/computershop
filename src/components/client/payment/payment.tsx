c4e84763eddfed8f25e38f768599970b0be70c36
dianaomr <dian.omrani@gmail.com>
2025-05-06 20:13:24 +0430

    fix: update peyment page

--- src/components/client/payment/payment.tsx
+++ src/components/client/payment/payment.tsx
@@ -1,15 +1,60 @@
 
-
 'use client'
 
-import { useRouter } from 'next/navigation'
-import styles from './PaymentMock.module.scss'
+import { useRouter } from 'next/navigation';
+import styles from './PaymentMock.module.scss';
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "@/redux/store";
 import { clearCart } from "@/redux/slices/cartSlice";
+import { createOrder } from "@/utils/orderService"; 
+import { setLatestOrder } from "@/redux/slices/latestOrderSlice";
+
 
 const PaymentPage = () => {
-  
+  const cart = useSelector((state: RootState) => state.cart.items);
+  const dispatch = useDispatch();
+  const router = useRouter();
+
+  const handlePay = async () => {
+    const customer = JSON.parse(localStorage.getItem("customer") || "{}");
+
+    if (cart.length && customer.name) {
+      const items = cart.map(item => ({
+        product: {
+          id: item.product.id || "", 
+          name: item.product.name
+        },
+        quantity: item.quantity
+      }));
+
+      const orderData = {
+        customer,
+        items,
+        total: cart.reduce(
+          (acc, item) => acc + item.quantity * item.product.price,
+          0
+        ),
+        createdAt: new Date().toISOString(),
+        delivered: false,
+        deliveredAt: undefined ,
+      };
+
+      try {
+        await createOrder(orderData);
+        dispatch(setLatestOrder(orderData));
+
+        dispatch(clearCart());
+        router.push("/client/payment-succes");
+      } catch (error) {
+        console.error("ثبت سفارش با خطا مواجه شد:", error);
+      }
+    }
+  };
+
+  const handleCancel = () => {
+    router.push('/client/payment-fail');
+  };
+
   return (
     <div className={styles.container}>
       <img
@@ -17,17 +62,17 @@ const PaymentPage = () => {
         alt="درگاه پرداخت"
         className={styles.mockImage}
       />
-
       <div className={styles.buttons}>
-        <button >
+        <button className={styles.payButton} onClick={handlePay}>
           پرداخت
         </button>
-        <button >
+        <button className={styles.cancelButton} onClick={handleCancel}>
           انصراف
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default PaymentPage
+export default PaymentPage;
+
