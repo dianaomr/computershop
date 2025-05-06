aaecf95053e3e09b5b5a46f5497efd86f4db30d5
dianaomr <dian.omrani@gmail.com>
2025-05-06 18:28:33 +0430

    fix: changes about main layout & add toaster

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,16 @@
+import { Toaster } from "react-hot-toast";
+
 // app/layout.tsx
 export const metadata = {
-    title: 'سایت من',
-    description: 'یک پروژه با دو بخش کلاینت و ادمین',
+    title: ' ایران تایمر',
+    description: ' پروژه با دو بخش کلاینت و ادمین',
   };
   
   export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
       <html lang="fa" dir="rtl">
         <body>{children}</body>
+        <Toaster/>
       </html>
     );
   }
