4b897392ce6b5aa0c92a6d876722fc5c28c6fa09
dianaomr <dian.omrani@gmail.com>
2025-06-30 23:23:26 +0430

    fix:delete useless file

--- src/app/client/page.tsx
+++
@@ -1,9 +0,0 @@
-import Home from "@/components/client/home/home";
-export const metadata = {
-  title: " خانه",
-  description: "صفحات عمومی سایت",
-};
-
-export default function HomePage() {
-  return <Home />;
-}
