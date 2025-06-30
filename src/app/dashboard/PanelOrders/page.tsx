53e87eafa269082a36ef0c67ac066eaff2fea06e
dianaomr <dian.omrani@gmail.com>
2025-06-30 23:31:40 +0430

    fix: add metadata title

--- src/app/dashboard/PanelOrders/page.tsx
+++ src/app/dashboard/PanelOrders/page.tsx
@@ -1,5 +1,8 @@
 import PanelOrders from '@/components/admin/panel/panel-orders';
-
+export const metadata = {
+  title: "پنل مدیریت",
+  description: "مدیریت محصولات، سفارشات و کاربران",
+};
 export default function PanelOrdersPage() {
   return <PanelOrders />;
 }
