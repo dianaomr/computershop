cc8bb3cc16084e5c897032a68d4877a3a3dfe163
dianaomr <dian.omrani@gmail.com>
2025-06-30 23:45:02 +0430

    fix: important problem fix

--- src/components/common/navbar.tsx
+++ src/components/common/navbar.tsx
@@ -13,7 +13,7 @@ import { fetchProducts, Product } from "@/utils/productService";
 
 const Navbar: React.FC = () => {
   const [menuOpen, setMenuOpen] = useState(false);
-
+  const cart = useSelector((state: RootState) => state.cart);
   const dispatch = useDispatch();
   const filters = useSelector((state: RootState) => state.filters);
   const [showDropdown, setShowDropdown] = useState(false);
@@ -22,6 +22,11 @@ const Navbar: React.FC = () => {
     queryKey: ["products"],
     queryFn: fetchProducts,
   });
+
+  const formatPrice = (num: number): string => {
+    const withCommas = num.toLocaleString("en-US");
+    return withCommas.replace(/\d/g, (d) => "۰۱۲۳۴۵۶۷۸۹"[parseInt(d)]);
+  };
 
   const filteredProducts = useMemo(() => {
     if (!data?.records) return [];
@@ -59,21 +64,21 @@ const Navbar: React.FC = () => {
               if (filteredProducts.length > 0) setShowDropdown(true);
             }}
             onBlur={() => setTimeout(() => setShowDropdown(false), 200)}
-            className="border w-full h-10 rounded-3xl px-4"
+            className="search-input border w-full h-10 rounded-3xl px-4 text-white "
           />
 
           {showDropdown && (
-            <div className="absolute top-12 w-full bg-white shadow-xl rounded-xl border max-h-80 overflow-y-auto z-50">
+            <div className="search-drop absolute top-12 w-full bg-white shadow-xl rounded-xl border max-h-80 overflow-y-auto z-50">
               {filteredProducts.map((item) => (
                 <Link
                   key={item.id}
                   href={`/product/${item.id}`}
-                  className="flex items-center gap-4 p-2 border-b hover:bg-gray-100 transition"
+                  className="flex items-center gap-4 p-2 border-b hover:bg-black hover:text-white transition"
                 >
                   <img src={item.image} alt={item.name} className="w-12 h-12 object-cover rounded-md" />
                   <div className="flex flex-col text-sm">
-                    <span className="font-medium text-black">{item.name}</span>
-                    <span className="text-green-600 font-semibold">{item.price?.toLocaleString()} تومان</span>
+                    <span className="font-medium text-blue  ">{item.name}</span>
+                    <span className="text-green-600 font-semibold  ">{item.price?.toLocaleString()} تومان</span>
                   </div>
                 </Link>
               ))}
@@ -100,16 +105,25 @@ const Navbar: React.FC = () => {
 
           {/* Desktop Menu */}
           <ul className="navbar hidden md:flex space-x-10 text-lg">
-            <li><Link href="/client">خانه</Link></li>
+            <li><Link href="/">خانه</Link></li>
             <li><Link href="/client/products">محصولات</Link></li>
             <li><Link href="/client/brands">برند ها</Link></li>
             <li><Link href="/client/aboutus">درباره ما</Link></li>
             <li><Link href="/client/contactus">تماس با ما</Link></li>
-            <li><Link href="/client/cart">سبد خرید</Link></li>
           </ul>
           <div className="flex justify-between gap-4">
-          <p><Link href="/client/cart"><ShoppingCart size={32}/></Link></p>
-          <p><Link href="/client/favorites"><FaHeart size={32} className="FaHeart cursor-pointer hover:scale-110 transition"/></Link></p>
+          <div className="relative">
+  <Link href="/client/cart">
+    <ShoppingCart size={30} />
+    {cart.items.length > 0 && (
+      <span className="absolute -top-2 -right-2 bg-black text-white text-xs rounded-full w-4 h-4 flex items-center justify-center">
+        {formatPrice(cart.items.length)}
+      </span>
+    )}
+  </Link>
+</div>
+
+          <p><Link href="/client/favorites"><FaHeart size={29} className="FaHeart cursor-pointer hover:scale-110 transition"/></Link></p>
           <button><Link href="/client/login">ورود / ثبت نام</Link></button>
         </div>
           </div>
@@ -118,7 +132,7 @@ const Navbar: React.FC = () => {
         {/* Mobile Menu */}
         {menuOpen && (
           <ul className="navbar md:hidden flex flex-col bg-black px-4 pb-4 space-y-4 text-lg">
-            <li><Link href="/client">خانه</Link></li>
+            <li><Link href="/">خانه</Link></li>
             <li><Link href="/client/products">محصولات</Link></li>
             <li><Link href="/client/aboutus">درباره ما</Link></li>
             <li><Link href="/client/contactus">تماس با ما</Link></li>
